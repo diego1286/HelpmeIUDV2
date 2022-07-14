37005cacabdd080fc8ab78a59e36da84d1802a40
diego1286 <diego.ardilas2486@gmail.com>
2022-07-14 15:29:21 -0500

    se agregan los primeros archivos y de la bd

---
+++ src/main/java/com/co/ed/iudigital/BackendHelpmedV2Application.java
@@ -0,0 +1,13 @@
+package com.co.ed.iudigital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendHelpmedV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendHelpmedV2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/co/ed/iudigital/BackendHelpmedV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.co.ed.iudigital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendHelpmedV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
